0e150a23d32f3c70d7369c50d0969fd6930a66fd
Farhan Azyumardhi Azmi <sonofkyrat99@gmail.com>
2019-11-12 21:37:28 +0700

    Added redirection from root URL to noise log list page

--- noise_log/urls.py
+++ noise_log/urls.py
@@ -5,5 +5,5 @@ from noise_log.views import noise_logs_list, CreateNoiseLog
 app_name = 'noise_log'
 urlpatterns = [
     path('all/', noise_logs_list, name='noise-logs-list'),
-    path('create-noise-log/', CreateNoiseLog.as_view(), name='create-noisse-log'),
+    path('create-noise-log/', CreateNoiseLog.as_view(), name='create-noise-log'),
 ]
